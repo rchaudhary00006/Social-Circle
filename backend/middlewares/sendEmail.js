653e51b3e152f052279abf747a811816b81fc96d
Arpit-Gamit <arpit02gamit07@gmail.com>
2023-11-18 09:59:07 +0530

    first commit

---
+++ backend/middlewares/sendEmail.js
@@ -0,0 +1,21 @@
+const nodeMailer = require("nodemailer");
+
+exports.sendEmail = async (options) => {
+  var transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "b1dcc9aac1f654",
+      pass: "176f95bc8fe4ec"
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
